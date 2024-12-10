0d77ae9a5a900d1d80d96666e931b64385123e3f
szata <szata@dtu.dk>
2024-12-10 16:34:55 +0100

    output paths corrected. scenarios for simple study file generated.

--- PipelineDispatcher.py
+++ PipelineDispatcher.py
@@ -1,9 +1,10 @@
 import yaml
 import subprocess
 import os
-import glob
-import json
 from pathlib import Path
+from time import time
+from itertools import product
+
 
 class PipelineDispatcher:
     def __init__(self, study_file_Nm, config_file_Nm):
@@ -62,34 +63,47 @@ class PipelineDispatcher:
         self.study_data = self.read_yaml(self.study_file_Nm, self.path_dispatcher)
 
 
-    def update_conf_run(self, conf_run, configurable_values):
-        for key, value in configurable_values.items():
-            if isinstance(value, dict):
-                self.update_conf_run(conf_run[key], value)
-            else:
-                if value not in ['default_value', 'default_name', 'default_list']:
-                    conf_run[key] = value
-                
-    def generate_run_configs(self):
-        
-        
-        config= self.read_yaml(self.config_file_Nm, self.path_simulation)
-        for run_key, configurable_values in self.study_data.items():
-            if run_key.startswith('run_'):
-                conf_run = config.copy()
-                self.update_conf_run(conf_run, configurable_values)
-                with open(f'conf_{run_key}.yaml', 'w') as f:
-                    yaml.safe_dump(conf_run, f)
-                    print(f"Generated config files for {run_key}.")
+    def generate_combinations(self, param_ranges):
+    # Flatten the nested dictionary into a flat dictionary with hierarchical keys
+        flat_params = {}
+        for outer_key, inner_dict in param_ranges.items():
+            for inner_key, values in inner_dict.items():
+                flat_params[f"{outer_key}.{inner_key}"] = values
+
+        # Generate Cartesian product of all parameter value lists
+        param_keys = list(flat_params.keys())
+        param_values = list(flat_params.values())
+        combinations = list(product(*param_values))
+
+        # Convert combinations into a list of dictionaries with hierarchical keys
+        scenarios = [dict(zip(param_keys, combo)) for combo in combinations]
+
+        return scenarios
+    def generate_scenarios(self):
+        param_ranges = self.study_data['study_param_range']
+        print(f"Generating scenarios for the following parameter ranges: {param_ranges}")
+        scenarios = self.generate_combinations(param_ranges)
+        study_run_dicts= self.study_data['study_run_dicts']
+        scenario_id = 0
+        for run_name, run_values in study_run_dicts.items():
+            for idx, scenario in enumerate(scenarios):
+                scenario_id += 1
+                print(f"Generating scenario {scenario_id} for run {run_name}")
+                with open(f'{self.Output_directory}/scenario_{scenario_id}.yaml', 'w') as f:
+                    config = scenario.copy()
+                    #TODO: Check if this is the correct way to update the config dictionary
+                    #TODO: check flattened hierarchical keys
+                    
 
     def execute_optimization(self, run_id):
         pass
     
-    def execute_simulation(self, run_id):
-        OUTyamlNmTxt= f'conf_{run_id}.yaml'
+    def execute_simulation(self, run_id, OUTdir_study):
+        OUTyamlNmTxt= f'conf_{run_id}'
         print(f'simulation is running for file: {OUTyamlNmTxt}')
-        OUTdir = f'conf_{run_id}_output'
         
+        
+
         matlab_script = f"""
             clear all; restoredefaultpath %%%%%%%%%%%%%%%
             clearvars -except INstruct; restoredefaultpath
@@ -102,12 +116,11 @@ class PipelineDispatcher:
             UTfile  = '';
             plotON  = 1;
             cfgON   = 1; 
-
-            INfile = [caseNm '.xlsx']; INdir = [caseNm,'_input']; 
-
-            [out]=t32_RefCase_RunSlx_4yalm2out('{OUTyamlNmTxt}','{OUTdir}','{self.MDLfile}',cfgON,plotON,UTfile);
-
-
+            OUTdir  = ['..\\log_data\\{OUTdir_study}\\{OUTyamlNmTxt}'];
+            INfile = [caseNm '.xlsx']; 
+            INdir = [caseNm,'_input']; 
+            OUTnm= 'Output';
+            [out]=t32_RefCase_RunSlx_4yalm2out('{OUTyamlNmTxt}.yaml',OUTdir,OUTnm,'{self.MDLfile}',cfgON,plotON,UTfile);
             """
         P2 = subprocess.Popen(
             ['matlab', '-batch', matlab_script], 
@@ -126,27 +139,14 @@ class PipelineDispatcher:
             print("Error in simulation")
         return P2.returncode
     
-    def calculate_kpis(self, run_id):
-        # Define the folder path and pattern
-        dir = f'{self.path_simulation}\\conf_{run_id}_output'
-        file_pattern = f'{dir}/OUT_*_KPI.json'
-        print(dir, file_pattern)
-        # Find the file
-        file_list = glob.glob(file_pattern)
-
-        if file_list:
-            # Load the first matching file
-            with open(file_list[0], 'r') as file:
-                file_path = file_list[0]
-                file_name = os.path.basename(file_path)
-                print(f"Found file: {file_name}")
-        else:
-                print(f"No matching file found for pattern: {file_pattern}")
-        print(f'\n \n Calculating KPIs for file: {file_name} and run: {run_id}\n \n')
+    def calculate_kpis(self, run_id, OUTdir_study):
+        print(f'KPI calculation is running for file: {run_id}')
+        script_parent_dir = Path(__file__).parent.parent
+        path = script_parent_dir / 'log_data'/ OUTdir_study/f'conf_{run_id}'
+        print(f'path: {path}')
         kpi_script_path = os.path.join(self.path_kpi_calculation, 'KPI_evaluation.py')
-        subprocess.Popen(['python', kpi_script_path, file_name, dir, run_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        
-    
+        subprocess.Popen(['python', kpi_script_path, 'Output_KPI',path, run_id])
+           
     def batch_kpi_calculation(self):
         pass
 
@@ -156,22 +156,31 @@ class PipelineDispatcher:
                 if run_key.startswith('run_'):
                     self.runs.append(run_key)
 
+# ********************************************************************************************************************
     def run_pipeline(self):
-        # Step 1: Convert XLS to YAML using MATLAB function
-        self.xls_to_yaml()
-        
-        # Step 2: Load Study Configuration
+        OUTdir_study = f'Study_{time():.00f}'
+
+        script_parent_dir = Path(__file__).parent.parent
+        log_data = script_parent_dir / 'log_data'
+        os.mkdir(log_data / OUTdir_study)
+        self.Output_directory = log_data / OUTdir_study
+        # Step 1: Load Study Configuration
         self.load_study()
         print('Study loaded')
+       
+        # Step 2: Convert XLS to YAML using MATLAB function
+        # self.xls_to_yaml()
         
-        # # Step 3: Generate Configuration Files for Each Run
-        self.generate_run_configs()
+        # Step 3: Generate Configuration Files for Each Run
+        self.generate_scenarios()
+        
+
         # Step 3.1: Get Run Keys
         self.run_keys()
 
         print('Runs generated')
         print(f'runs: {self.runs}')
-
+        
         # Step 4: Run Optimization and Simulation for Each Run
         for run_id in self.runs:
 
@@ -183,19 +192,19 @@ class PipelineDispatcher:
                 print(f"Optimization completed for run {run_id}")
 
             # Step 4.2: Run Simulation
-            if self.execute_simulation(run_id) != 0:
+            if self.execute_simulation(run_id, OUTdir_study) != 0:
                 print(f"Simulation failed for run {run_id}")
             else:
                 print(f"Simulation completed for run {run_id}")
             
             # Step 5: Calculate KPIs for Each Run
-            self.calculate_kpis(run_id)
+            self.calculate_kpis(run_id, OUTdir_study)
         
         # Step 6: Calculate Batch KPIs
         self.batch_kpi_calculation()
 
 # Example usage
 if __name__ == "__main__":
-    dispatcher = PipelineDispatcher(study_file_Nm="study", 
-                                    config_file_Nm="StoRIES_RefCase_Config_rev04")
+    dispatcher = PipelineDispatcher(study_file_Nm="study_simple", 
+                                    config_file_Nm="StoRIES_RefCase_Config_Simple")
     dispatcher.run_pipeline()
